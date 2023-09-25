670ba3b837be8c6575177c9e602627c2ecc64b3d
DimaSudenkov <sudenkovdimas@gmail.com>
2023-09-25 15:52:38 +0300

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,19 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+const button = document.querySelector('.change-color');
+const body = document.querySelector('body');
+const span = document.querySelector('.color')
+
+button.addEventListener('click', handlerButton);
+
+function handlerButton() {
+  function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, 0)}`;
+  }
+  const fun = getRandomHexColor();
+  console.log(fun);
+
+  span.textContent = fun;
+  body.style.backgroundColor = fun;
 }
+
